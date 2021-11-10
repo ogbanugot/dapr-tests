177d612b6183b691ecddca1dbd960f3e686a2ab9
ogbanugot <ogbanugot@gmail.com>
2021-11-10 13:46:34 +0100

    add mask feature

--- pythonapp2/app.py
+++ pythonapp2/app.py
@@ -9,10 +9,11 @@ rule = {
     "name": Rule(type=str, gte=2, dest="Name")
 }
 
-@app.route("/testpythonapps", methods=['POST'])
-def incoming():
+@app.route("/testpythonapps")
+def incoming(request, context):
     print("Hello from Kafka!", flush=True)
-    data = request.get_json()
+    params = pre.parse(rule=rule, request=request, context=context)
+    data = params['Name']
     print(data, flush=True)
     return(data)
 
